b0d197f94eb63d3ca64fef58e724215fd3e3e0d0
irem.alagoz@boun.edu.tr <46854279+irem-zeynep@users.noreply.github.com>
2020-06-28 00:01:12 +0300

    Create Borrow.java

---
+++ Cmpe160/Project1/LibrarySimulator/interfaces/Borrow.java
@@ -0,0 +1,12 @@
+package interfaces;
+
+import librarymembers.LibraryMember;
+
+
+public interface Borrow {
+	
+	
+	public void borrowBook(LibraryMember member, int tick); 
+	
+	
+	public void extend(LibraryMember member, int tick); } 
